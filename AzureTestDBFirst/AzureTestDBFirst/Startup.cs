359851c44cdbe69bc6a0f4d2adf0c10fe56a12c4
Mark <mesmit22@louisville.edu>
2016-04-07 16:44:41 -0400

    Init source code

---
+++ AzureTestDBFirst/AzureTestDBFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureTestDBFirst.Startup))]
+namespace AzureTestDBFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
